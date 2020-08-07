4c6616cd38aed4a8296569110a77e51935e031c5
ln-sergey <lnov.sergey@gmail.com>
2020-08-08 00:10:31 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/lnsergey/maxfit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lnsergey.maxfit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:maxfit/screens/HomeScreen.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Maxfit',
+        theme: ThemeData(
+          primaryColor: Color.fromRGBO(50, 65, 85, 1),
+          textTheme: TextTheme(headline1: TextStyle(color: Colors.white)),
+        ),
+        home: HomeScreen(),
+      );
+}
